8361ee0a5629dd433f24989fc86c5990739ef917
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-05-30 13:19:38 +0530

    [setup]:files and folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "../pages/Home"
+import Login from "../pages/Login"
+import Register from "../pages/Register"
+
+function App() {
+
+
+  return (
+    <>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+    </>
+  )
+}
+
+export default App
